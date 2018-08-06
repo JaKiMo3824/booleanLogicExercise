733b3a3dc510b8d883391016af56df348f9021ac
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-06 12:20:41 -0700

    booleanLogicExercise

--- booleanLogicExercise/Program.cs
+++ booleanLogicExercise/Program.cs
@@ -10,6 +10,30 @@ namespace booleanLogicExercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please answer the following questions in order to determine whether you qualify for car insurance: ");
+            Console.WriteLine("What is your age?");
+            string customerAge = Console.ReadLine();
+            int custAge = Convert.ToInt32(customerAge);
+
+            Console.WriteLine("Have you ever had a DUI? Answer TRUE or FALSE");
+            string customerDui = Console.ReadLine();
+            bool custDui = Convert.ToBoolean(customerDui);
+
+            Console.WriteLine("How many speeding tickets do you have? ");
+            string customerTickets = Console.ReadLine();
+            int custTic = Convert.ToInt32(customerTickets);
+
+            bool age = custAge > 15;
+            bool dui = custDui == false;
+            bool tic = custTic <= 3;
+
+            bool qualify = age && dui && tic;
+
+            Console.WriteLine("Do you qualify? ");
+            Console.WriteLine(qualify);
+            Console.ReadLine();
+          
+
         }
     }
 }
